ab855b13fbcde89b44918ff8842bc133030449a2
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-10-12 09:30:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ PORT=${PORT:-5000}
 
 sleep ${PAUSE:-0}
 
+#exec  java -Dserver.port=$PORT $JAVA_OPTS $JAVA_ADD_OPTS -jar target/*.jar
 exec  java -Dserver.port=$PORT $JAVA_OPTS $JAVA_ADD_OPTS -jar target/*.jar
